4034efc425a791f4e1d1c7a777458f58f8d62b56
npablo1987 <kyovilches@gmail.com>
2023-01-08 22:13:14 -0400

    Configurando web SecurityConfig

---
+++ src/main/java/com.ejercicio.pvilches.app.pvilchesjwt.security/WebSecutiryConfig.java
@@ -0,0 +1,62 @@
+package com.ejercicio.pvilches.app.pvilchesjwt.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecutiryConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager){
+        return http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService(){
+        InMemoryUserDetailsManager magener = new InMemoryUserDetailsManager();
+        magener.createUser(User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles()
+                .build());
+        return magener;
+
+    }
+
+    @Bean
+    AuthenticationManager authManager(HttpSecurity http) throws Exception{
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
+    }
+
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+
+    }
+
+
+
+}
